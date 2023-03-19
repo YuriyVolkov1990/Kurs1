39d24ff7c8f62e799c94fbdbf17c49dc6405698d
Professional <new99kpicem@gmail.com>
2023-03-19 16:04:21 +0300

    work i dont understand

--- src/pro/sky/java/course1/courseWork/Main.java
+++ src/pro/sky/java/course1/courseWork/Main.java
@@ -7,23 +7,29 @@ public class Main {
 
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("Иванов Иван Иванович", 1,1);
-        employees[1] = new Employee("Петров Иван Иванович", 2,2);
-        employees[2] = new Employee("Сидоров Иван Иванович", 3,3);
-        employees[3] = new Employee("Пвыаып Иван Иванович", 4,4);
-        employees[4] = new Employee("Квсмстпее Иван Иванович", 5,5);
-        employees[5] = new Employee("Аиагрпор Иван Иванович", 1,6);
-        employees[6] = new Employee("Аврьи Иван Иванович", 2,7);
-        employees[7] = new Employee("Нгшглобр Иван Иванович", 3,8);
-        employees[8] = new Employee("Уцевыва Иван Иванович", 4,9);
-        employees[9] = new Employee("Акенп Иван Иванович", 5,10);
-        System.out.println(Arrays.toString(employees));
+        employees[0] = new Employee("Иванов Иван Иванович", 1, 1);
+        employees[1] = new Employee("Петров Иван Иванович", 2, 2);
+        employees[2] = new Employee("Сидоров Иван Иванович", 3, 3);
+        employees[3] = new Employee("Пвыаып Иван Иванович", 4, 4);
+        employees[4] = new Employee("Квсмстпее Иван Иванович", 5, 5);
+        employees[5] = new Employee("Аиагрпор Иван Иванович", 1, 6);
+        employees[6] = new Employee("Аврьи Иван Иванович", 2, 7);
+        employees[7] = new Employee("Нгшглобр Иван Иванович", 3, 8);
+        employees[8] = new Employee("Уцевыва Иван Иванович", 4, 9);
+        employees[9] = new Employee("Акенп Иван Иванович", 5, 10);
+        System.out.println(Arrays.toString(employees));//в задании сказано создать статический метод toString, но это метод объекта, разве он может быть static? и Java ругается если попытаться сделать его static
         System.out.println("Сотрудник " + employees[0].getPerson() + " из отдела " + employees[0].getDepart() + " переведён в отдел " + employees[0].setDepart(10));
-        System.out.println("Сотруднику " + employees[1].getPerson() + " назначена зарплата " + employees[1].setSalary(0) + " рубль");
+        System.out.println("Сотруднику " + employees[1].getPerson() + " назначена зарплата " + employees[1].setSalary(2) + " р.");
         int sum = 0;
-        for (int i =0; i<=9;i++) {
-            sum+=employees[i].getSalary();
+        int maxSalary = 0;
+        for (int i = 0; i <= 9; i++) {
+            sum += employees[i].getSalary();
         }
         System.out.println(sum);
+
+        for (int i = 0; i <= 8; i++) {
+            maxSalary = Math.max(employees[i].getSalary(), employees[i + 1].getSalary());
+        }
+        System.out.println(maxSalary);
     }
-    }
+}

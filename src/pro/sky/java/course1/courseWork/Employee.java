ad01261bc0a3d42683fb3d2d263d71e55b50f4ec
Professional <new99kpicem@gmail.com>
2023-03-19 15:32:40 +0300

    work i dont understand

--- src/pro/sky/java/course1/courseWork/Employee.java
+++ src/pro/sky/java/course1/courseWork/Employee.java
@@ -6,40 +6,46 @@ public class Employee {
     int salary;//зарплата
     int id;//id сотрудника
 
-public String getPerson() {
-    return person;
-}
-public int getDepart() {
-    return depart;
-}
-public int getSalary() {
-    return salary;
-}
-public int getId() {
-    return id;
-}
-public int setDepart(int depart) {
-    this.depart = depart;
-    return depart;
-}
-public int setSalary(int salary) {
-    this.salary = salary;
-    return salary;
-}
-public Employee(String person, int depart, int salary) {
-    this.person = person;
-    this.depart = depart;
-    this.salary = salary;
-    this.id = Main.counterId++;
+    public String getPerson() {
+        return person;
+    }
+
+    public int getDepart() {
+        return depart;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int setDepart(int depart) {
+        this.depart = depart;
+        return depart;
+    }
+
+    public int setSalary(int salary) {
+        this.salary = salary;
+        return salary;
+    }
+
+    public Employee(String person, int depart, int salary) {
+        this.person = person;
+        this.depart = depart;
+        this.salary = salary;
+        this.id = Main.counterId++;
     }
 
     @Override
     public String toString() {
         return "Сотрудик {" +
-                "ФИО='" + getPerson() + '\'' +
-                ", Отдел=" + getDepart() +
-                ", Зарплата=" + getSalary() +
-                ", id=" + getId() +
+                "ФИО='" + person + '\'' +
+                ", Отдел=" + depart +
+                ", Зарплата=" + salary +
+                ", id=" + id +
                 '}';
     }
 }
--- src/pro/sky/java/course1/courseWork/Main.java
+++ src/pro/sky/java/course1/courseWork/Main.java
@@ -4,20 +4,26 @@ import java.util.Arrays;
 
 public class Main {
     public static int counterId = 0;
+
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee("Иванов Иван Иванович", 1,1000000);
-        employees[1] = new Employee("Петров Иван Иванович", 2,1000001);
-        employees[2] = new Employee("Сидоров Иван Иванович", 3,1000002);
-        employees[3] = new Employee("Пвыаып Иван Иванович", 4,1000003);
-        employees[4] = new Employee("Квсмстпее Иван Иванович", 5,1000004);
-        employees[5] = new Employee("Аиагрпор Иван Иванович", 1,1000005);
-        employees[6] = new Employee("Аврьи Иван Иванович", 2,1000006);
-        employees[7] = new Employee("Нгшглобр Иван Иванович", 3,1000007);
-        employees[8] = new Employee("Уцевыва Иван Иванович", 4,1000008);
-        employees[9] = new Employee("Акенп Иван Иванович", 5,1000009);
+        employees[0] = new Employee("Иванов Иван Иванович", 1,1);
+        employees[1] = new Employee("Петров Иван Иванович", 2,2);
+        employees[2] = new Employee("Сидоров Иван Иванович", 3,3);
+        employees[3] = new Employee("Пвыаып Иван Иванович", 4,4);
+        employees[4] = new Employee("Квсмстпее Иван Иванович", 5,5);
+        employees[5] = new Employee("Аиагрпор Иван Иванович", 1,6);
+        employees[6] = new Employee("Аврьи Иван Иванович", 2,7);
+        employees[7] = new Employee("Нгшглобр Иван Иванович", 3,8);
+        employees[8] = new Employee("Уцевыва Иван Иванович", 4,9);
+        employees[9] = new Employee("Акенп Иван Иванович", 5,10);
         System.out.println(Arrays.toString(employees));
         System.out.println("Сотрудник " + employees[0].getPerson() + " из отдела " + employees[0].getDepart() + " переведён в отдел " + employees[0].setDepart(10));
-        System.out.println("Сотруднику " + employees[1].getPerson() + " назначена зарплата " + employees[1].setSalary(1) + " рубль");
+        System.out.println("Сотруднику " + employees[1].getPerson() + " назначена зарплата " + employees[1].setSalary(0) + " рубль");
+        int sum = 0;
+        for (int i =0; i<=9;i++) {
+            sum+=employees[i].getSalary();
+        }
+        System.out.println(sum);
     }
- }
+    }
